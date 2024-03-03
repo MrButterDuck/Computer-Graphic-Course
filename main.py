a2b83cdd3da747dbf5fe42775001cd30731da32b
MrButterDuck <anton.kostyuchencko2016@gmail.com>
2024-03-03 18:03:39 +1000

    started project

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame as pg
+
+class Engine():
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+    def handle_events(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    app = Engine()
+    app.run()
